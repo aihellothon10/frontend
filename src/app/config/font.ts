70c6b476ec4194842f60d39536987005adb8d707
LEE DONG GYU <tenenger7125@gmail.com>
2024-11-20 10:41:49 +0900

    feat: add locallay nanum-square font

--- src/app/config/font.ts
+++ src/app/config/font.ts
@@ -4,3 +4,8 @@ export const wantedSansFont = localFont({
   src: '../../assets/fonts/WantedSansVariable.woff2',
   variable: '--font-wanted-sans',
 });
+
+export const nanumSquareEBFont = localFont({
+  src: '../../assets/fonts/NanumSquareEB.otf',
+  variable: '--font-nanum-square-extra-bold',
+});
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
-import { wantedSansFont } from '@/app/config/font';
+import { nanumSquareEBFont, wantedSansFont } from '@/app/config/font';
 import { metadata } from '@/app/config/metadata';
 
 import '@/styles/globals.css';
 
 const RootLayout = ({ children }: RootLayoutProps) => (
   <html lang="ko">
-    <body className={`${wantedSansFont.className}`}>{children}</body>
+    <body className={`${wantedSansFont.className} ${nanumSquareEBFont.variable}`}>{children}</body>
   </html>
 );
 
