8048789461837074c42d78f8eb8c7003740b719c
LEE DONG GYU <tenenger7125@gmail.com>
2024-11-20 21:37:02 +0900

    fix: add Image component size attr

--- src/app/memo/_components/MemoCards/MemoCard.tsx
+++ src/app/memo/_components/MemoCards/MemoCard.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 const MemoCard = ({ isError }: MemoCardProps) => (
   <div className="flex justify-between p-4">
-    <div className="gap-5 flex flex-col text-grayscale-60">
+    <div className="flex flex-col gap-5 text-grayscale-60">
       <p className="flex gap-6 label-sm-12-medium">
         {isError && (
           <>
@@ -15,13 +15,13 @@ const MemoCard = ({ isError }: MemoCardProps) => (
         <span>어제</span>
       </p>
       <p className="text-body-sm text-grayscale-80">메모 제목</p>
-      <p className="gap-5 flex items-center">
+      <p className="flex items-center gap-5">
         <span className="icon-xs">summarize</span>
         <span className="label-sm-12-medium">메모 내용 요약된 텍스트 한 줄</span>
       </p>
     </div>
     <div className="relative h-11 w-11 border border-grayscale-30">
-      <Image alt="image" className="object-cover" src="/images/baby_mock.png" fill />
+      <Image alt="image" className="object-cover" sizes="11rem" src="/images/baby_mock.png" fill />
     </div>
   </div>
 );
