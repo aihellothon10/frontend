a34fb2e06cbd20905247ab974499fed46ce54116
LEE DONG GYU <tenenger7125@gmail.com>
2024-11-20 21:35:32 +0900

    feat: add drag memo chip feature

--- src/app/memo/_components/MemoChips/MemoChips.tsx
+++ src/app/memo/_components/MemoChips/MemoChips.tsx
@@ -1,9 +1,48 @@
-import { useState } from 'react';
+import { useCallback, useRef, useState } from 'react';
 
 import MemoChip from './MemoChip';
 
 const MemoChips = () => {
   const [filter, setFilter] = useState<Set<string>>(new Set());
+
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [isDragging, setIsDragging] = useState(false);
+  const [startX, setStartX] = useState(0);
+  const [totalX, setTotalX] = useState(0);
+
+  const preventSideEffect = useCallback((e: Event) => {
+    e.preventDefault();
+    e.stopPropagation();
+  }, []);
+
+  const onDragStart: React.MouseEventHandler<HTMLDivElement> = e => {
+    preventSideEffect(e.nativeEvent);
+
+    setIsDragging(true);
+    const x = e.clientX;
+    setStartX(x);
+    if (containerRef.current && 'scrollLeft' in containerRef.current) setTotalX(x + containerRef.current.scrollLeft);
+  };
+  const onDragMove: React.MouseEventHandler<HTMLDivElement> = e => {
+    preventSideEffect(e.nativeEvent);
+
+    if (!isDragging) return;
+
+    const scrollLeft = totalX - e.clientX;
+
+    if (containerRef.current && 'scrollLeft' in containerRef.current) containerRef.current.scrollLeft = scrollLeft;
+  };
+  const onDragEnd: React.MouseEventHandler<HTMLDivElement> = e => {
+    if (!isDragging) return;
+    if (!containerRef.current) return;
+
+    const endX = e.clientX;
+    const dragDiff = Math.abs(startX - endX);
+
+    if (dragDiff > 10) preventSideEffect(e.nativeEvent);
+
+    setIsDragging(false);
+  };
 
   const handleChipChange = (item: string) => {
     setFilter(prev => {
@@ -18,7 +57,15 @@ const MemoChips = () => {
 
   return (
     <>
-      <div className="mt-4 flex gap-6 overflow-x-hidden text-nowrap">
+      <div
+        ref={containerRef}
+        className="mt-4 flex gap-6 overflow-x-hidden text-nowrap"
+        role="button"
+        tabIndex={0}
+        onMouseDown={onDragStart}
+        onMouseLeave={onDragEnd}
+        onMouseMove={onDragMove}
+        onMouseUp={onDragEnd}>
         {items.map(item => (
           <MemoChip key={item} filter={filter} item={item} onChange={handleChipChange} />
         ))}
