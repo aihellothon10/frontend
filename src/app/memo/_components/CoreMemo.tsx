0948cddfce034f8ccd160d1c56e95f516bcb6ef0
LEE DONG GYU <tenenger7125@gmail.com>
2024-11-21 12:07:21 +0900

    style: modify CoreMemo layout style

--- src/app/memo/_components/CoreMemo.tsx
+++ src/app/memo/_components/CoreMemo.tsx
@@ -3,12 +3,16 @@ import { Button } from '@/app/_components/common';
 const CoreMemo = () => (
   <div className="flex gap-8">
     <Button className="bg-grayscale-20 py-3">
-      <span className="text-grayscale-60 label-md-14-regular">즐겨찾는 메모</span>
-      <span className="text-grayscale-50 icon-fill icon-xs">star</span>
+      <span className="inline-flex items-center gap-6">
+        <span className="text-grayscale-60 label-md-14-regular">즐겨찾는 메모</span>
+        <span className="text-grayscale-50 icon-xs">star</span>
+      </span>
     </Button>
     <Button className="bg-grayscale-20 py-3">
-      <span className="text-grayscale-60 label-md-14-regular">숨겨진 메모</span>
-      <span className="text-grayscale-50 icon-fill icon-xs">lock</span>
+      <span className="inline-flex items-center gap-6">
+        <span className="text-grayscale-60 label-md-14-regular">숨겨진 메모</span>
+        <span className="text-grayscale-50 icon-xs">lock</span>
+      </span>
     </Button>
   </div>
 );
