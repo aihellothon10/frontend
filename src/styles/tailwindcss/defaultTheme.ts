d969f0a7ed07a51e959357a04eb7e0f086af56fa
sihyunlights <62822977+sihyunlts@users.noreply.github.com>
2024-11-21 12:45:56 +0900

    style: Add more borderRadius variables

--- src/styles/tailwindcss/defaultTheme.ts
+++ src/styles/tailwindcss/defaultTheme.ts
@@ -65,11 +65,13 @@ export const defaultTheme: Config['theme'] = {
     24: '24px',
   },
   borderRadius: {
+    4: '4px',
     8: '8px',
     12: '12px',
     14: '14px',
     16: '16px',
     20: '20px',
+    76: '76px',
     max: '999px',
   },
   fontFamily: {
