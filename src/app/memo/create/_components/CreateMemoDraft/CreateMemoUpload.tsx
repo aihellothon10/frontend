4ddb819d2c4e4250df775b431a77375b3012e054
LEE DONG GYU <tenenger7125@gmail.com>
2024-11-22 01:18:27 +0900

    feat: add image upload feature

--- src/app/memo/create/_components/CreateMemoDraft/CreateMemoUpload.tsx
+++ src/app/memo/create/_components/CreateMemoDraft/CreateMemoUpload.tsx
@@ -1,33 +1,41 @@
-import { Button, Image } from '@/app/_components/common';
+import { Image } from '@/app/_components/common';
+import { useFileReader } from '@/app/hooks/common';
 
 import CreateMemoSection from '../CreateMemoSection';
 
-const CreateMemoUpload = () => (
-  <CreateMemoSection title="사진">
-    <div className="flex gap-8">
-      <Image
-        alt="thumbnail"
-        className="border border-grayscale-30"
-        containerClassName="w-18 h-18 rounded-14"
-        sizes="4.5rem"
-        src="/images/baby1_mock.png"
-      />
-      <Image
-        alt="thumbnail"
-        className="border border-grayscale-30"
-        containerClassName="w-18 h-18 rounded-14"
-        sizes="4.5rem"
-        src="/images/baby2_mock.png"
-      />
-      <div>
-        <Button className="h-18 w-18 p-0" fullWidth={false} onClick={() => {}}>
-          <span className="inline-flex items-center justify-center text-grayscale-50 icon-fill icon-lg">
-            add_photo_alternate
-          </span>
-        </Button>
+const CreateMemoUpload = () => {
+  const { images, reader } = useFileReader();
+
+  const handleImageChange: React.ChangeEventHandler<HTMLInputElement> = e => {
+    const files = [...(e.target?.files ?? [])] as File[];
+
+    [...files].forEach(file => reader(file));
+  };
+
+  return (
+    <CreateMemoSection title="사진">
+      <div className="flex gap-8">
+        {images.map((image, index) => (
+          <Image
+            key={index}
+            alt="thumbnail"
+            className="border border-grayscale-30"
+            containerClassName="w-18 h-18 rounded-14"
+            sizes="4.5rem"
+            src={image}
+          />
+        ))}
+        <div>
+          <label className="inline-flex h-18 w-18 cursor-pointer items-center justify-center rounded-14 border border-grayscale-30">
+            <input className="hidden" type="file" onChange={handleImageChange} />
+            <span className="inline-flex items-center justify-center text-grayscale-50 icon-fill icon-lg">
+              add_photo_alternate
+            </span>
+          </label>
+        </div>
       </div>
-    </div>
-  </CreateMemoSection>
-);
+    </CreateMemoSection>
+  );
+};
 
 export default CreateMemoUpload;
