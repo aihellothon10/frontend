ca422563009d4ad9fceb576917201bf4fa3e4235
LEE DONG GYU <tenenger7125@gmail.com>
2024-11-22 18:55:03 +0900

    comment: remove unnecessary comment

--- src/app/memo/[memoId]/page.tsx
+++ src/app/memo/[memoId]/page.tsx
@@ -1,16 +1,12 @@
 import { MemoHeader, MemoNote } from './_components';
 import MemoBottomFooter from './_components/MemoBottomFooter';
 
-const Memo = () => {
-  console.log('asdasd');
-
-  return (
-    <>
-      <MemoHeader />
-      <MemoNote />
-      <MemoBottomFooter />
-    </>
-  );
-};
+const Memo = () => (
+  <>
+    <MemoHeader />
+    <MemoNote />
+    <MemoBottomFooter />
+  </>
+);
 
 export default Memo;
