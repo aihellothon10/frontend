44423eebbab1196bc5015fba52c779a4f0f6d19f
LEE DONG GYU <tenenger7125@gmail.com>
2024-11-23 01:46:31 +0900

    refactor: add CoreMemo import

--- src/app/memo/_components/CoreMemo.tsx
+++ src/app/memo/_components/CoreMemo.tsx
@@ -2,13 +2,13 @@ import { Button } from '@/app/_components/common';
 
 const CoreMemo = () => (
   <div className="flex gap-8">
-    <Button className="bg-grayscale-20 py-3">
+    <Button className="bg-grayscale-20 py-3" onClick={() => {}}>
       <span className="inline-flex items-center gap-6">
         <span className="text-grayscale-60 body-sm-15-regular">즐겨찾는 메모</span>
         <span className="text-grayscale-50 icon-xs">star</span>
       </span>
     </Button>
-    <Button className="bg-grayscale-20 py-3">
+    <Button className="bg-grayscale-20 py-3" onClick={() => {}}>
       <span className="inline-flex items-center gap-6">
         <span className="text-grayscale-60 body-sm-15-regular">숨겨진 메모</span>
         <span className="text-grayscale-50 icon-xs">lock</span>
--- src/app/memo/_components/index.ts
+++ src/app/memo/_components/index.ts
@@ -3,3 +3,4 @@ export { default as MemoInput } from './MemoInput';
 export { default as AlertMemo } from './AlertMemo';
 export { default as MemoCards } from './MemoCards/MemoCards';
 export { default as MemoWarningCard } from './MemoWarningCard';
+export { default as CoreMemo } from './CoreMemo';
