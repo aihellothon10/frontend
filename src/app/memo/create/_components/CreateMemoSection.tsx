e2e03326c8cdac73d890e92045c9305d860b70f8
LEE DONG GYU <tenenger7125@gmail.com>
2024-11-23 14:26:33 +0900

    feat: modify CreateMemoSection to show modal

--- src/app/memo/create/_components/CreateMemoSection.tsx
+++ src/app/memo/create/_components/CreateMemoSection.tsx
@@ -1,16 +1,59 @@
-const CreateMemoSection = ({ title, help, children }: CreateMemoSectionProps) => (
-  <div className="flex flex-col gap-8">
-    <div className="flex gap-6">
-      <div className="text-grayscale-60 label-md-14-regular">{title}</div>
-      {help && <span className="text-grayscale-50 icon-xs">help</span>}
-    </div>
-    {children}
-  </div>
-);
+'use client';
+
+import { useDisClosure } from 'dotori-hooks';
+
+import { Modal } from '@/app/_components/common';
+
+const CreateMemoSection = ({
+  title,
+  help,
+  link,
+  children,
+  helpDescription,
+  linkDescription,
+}: CreateMemoSectionProps) => {
+  const { isOpen, open, close } = useDisClosure();
+
+  return (
+    <>
+      <div className="flex flex-col gap-8 label-md-14-regular">
+        <div className="flex justify-between gap-6">
+          <div>
+            <div className="text-grayscale-60">{title}</div>
+            {help && (
+              <button className="text-grayscale-50 icon-xs" onClick={open}>
+                help
+              </button>
+            )}
+          </div>
+          <button className="cursor-pointer text-blue-50 underline" onClick={open}>
+            {link}
+          </button>
+        </div>
+        {children}
+      </div>
+      {helpDescription && (
+        <Modal close={close} isOpen={isOpen}>
+          {helpDescription({ isOpen, open, close })}
+        </Modal>
+      )}
+      {linkDescription && (
+        <Modal close={close} isOpen={isOpen}>
+          {linkDescription({ isOpen, open, close })}
+        </Modal>
+      )}
+    </>
+  );
+};
+
+type UseDisclosure = (params: { isOpen: boolean; open: () => void; close: () => void }) => React.ReactNode;
 
 interface CreateMemoSectionProps {
   title: string;
   help?: boolean;
+  helpDescription?: UseDisclosure;
+  link?: string;
+  linkDescription?: UseDisclosure;
   children: React.ReactNode;
 }
 
--- src/app/memo/create/_components/index.ts
+++ src/app/memo/create/_components/index.ts
@@ -1,3 +1,4 @@
 export { default as CreateMemoHeader } from './CreateMemoHeader';
 export { default as CreateMemoStepper } from './CreateMemoStepper';
+export { default as CreateMemoSection } from './CreateMemoSection';
 export { default as CreateMemoContent } from './CreateMemoContent';
