86677aa5460b4fb40d81ea151db16e5d05f78343
LEE DONG GYU <tenenger7125@gmail.com>
2024-11-23 20:13:34 +0900

    refactor: add MemoWarningCard children prop

--- src/app/memo/[memoId]/_components/MemoNote/MemoNote.tsx
+++ src/app/memo/[memoId]/_components/MemoNote/MemoNote.tsx
@@ -9,7 +9,10 @@ const MemoNotes = () => (
   <>
     <div className="flex flex-col gap-20">
       <MemoNoteHeader />
-      <MemoWarningCard />
+      <MemoWarningCard>
+        분석 결과, 해당 메모에는 <span className="text-red-50">잘못된 정보</span>와{' '}
+        <span className="text-red-50">유해한 정보</span>가 포함되어 있을 수 있습니다.
+      </MemoWarningCard>
       <MemoNoteBody />
     </div>
   </>
--- src/app/memo/create/_components/CreateMemoSetting/CreateMemoSetting.tsx
+++ src/app/memo/create/_components/CreateMemoSetting/CreateMemoSetting.tsx
@@ -41,7 +41,10 @@ const CreateMemoSetting = () => (
   // console.log();
   <>
     <div className="flex flex-col gap-24 px-4 py-3">
-      <MemoWarningCard />
+      <MemoWarningCard>
+        분석 결과, 해당 메모에는 <span className="text-red-50">잘못된 정보</span>와{' '}
+        <span className="text-red-50">유해한 정보</span>가 포함되어 있을 수 있습니다.
+      </MemoWarningCard>
       <CreateMemoTitle />
       <CreateMemoNote />
       <CreateMemoConnection />
