9c387477df6a0e0bd170e94da20eccbab621eb3d
LEE DONG GYU <tenenger7125@gmail.com>
2024-11-23 14:40:42 +0900

    feat: add FamilyHeader

---
+++ src/app/family/_components/FamilyHeader.tsx
@@ -0,0 +1,12 @@
+import { Header } from '@/app/_components/common';
+
+const FamilyHeader = () => (
+  <Header>
+    <div className="flex w-full items-center gap-4 bg-grayscale-20 px-6 py-4.25">
+      <span className="icon-fill icon-md">diversity_4</span>
+      <span className="title-md-22-extrabold">가족</span>
+    </div>
+  </Header>
+);
+
+export default FamilyHeader;
---
+++ src/app/family/_components/index.ts
@@ -0,0 +1,1 @@
+export { default as FamilyHeader } from './FamilyHeader';
